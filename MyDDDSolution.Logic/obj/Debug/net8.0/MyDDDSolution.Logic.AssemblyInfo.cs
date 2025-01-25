c3ce4c3f5fb0641530b1b58ef72a018bf114281c
Demonically Possessed Exorcist Salman <peaceful.salman@yahoo.com>
2025-01-25 22:56:27 +0300

    added .gitignore file.

--- MyDDDSolution.Logic/obj/Debug/net8.0/MyDDDSolution.Logic.AssemblyInfo.cs
+++ MyDDDSolution.Logic/obj/Debug/net8.0/MyDDDSolution.Logic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDDDSolution.Logic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62155962eb28dfb77b7b538b5bbba2ec1db9cd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDDDSolution.Logic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDDDSolution.Logic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
